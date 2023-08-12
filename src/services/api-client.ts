2e87e446dfb8e5a39187dde0d1f71ac91e2543cb
robertocannella <robertocannella@gmail.com>
2023-08-12 15:17:48 -0400

    Add React Query

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "ac989381445a4337b583b66ae4d51b91",
   },
 });
